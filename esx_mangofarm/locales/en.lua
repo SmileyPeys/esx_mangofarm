7310b766bc19275ddaad01ccc8d4217dab8ab9fa
Smiley <95607967+SmileyPeys@users.noreply.github.com>
2022-03-02 18:58:59 -0800

    Update en.lua

--- esx_mangofarm/locales/en.lua
+++ esx_mangofarm/locales/en.lua
@@ -1,15 +1,15 @@
 Locales ['en'] = {
   -- weed
-  ['wood_pickupprompt'] = 'press ~INPUT_CONTEXT~ to  ~g~Search Apple~s~ .',
+  ['wood_pickupprompt'] = 'press ~INPUT_CONTEXT~ to  ~g~Search Mango~s~ .',
   ['wood_inventoryfull'] = 'Inventory Full.',
 
   -- etc
-  ['Enter'] = 'press ~INPUT_CONTEXT~ to Wash ~g~Apple~s~. ',
-  ['EnterSell'] = 'press ~INPUT_CONTEXT~ to Sell ~g~Fresh Apple~s~. ',
+  ['Enter'] = 'press ~INPUT_CONTEXT~ to Wash ~g~Mango~s~. ',
+  ['EnterSell'] = 'press ~INPUT_CONTEXT~ to Sell ~g~Fresh Mango~s~. ',
   ['NoItem'] = "You don't have this item.",
 
 -- blips
-  ['map_blipF'] = 'Apple Field',
-  ['map_blipW'] = 'Wash Apple',
-  ['map_blip'] = 'Apple',
+  ['map_blipF'] = 'Mango Field',
+  ['map_blipW'] = 'Wash Mango',
+  ['map_blip'] = 'Mango',
 }
