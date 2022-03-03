d574a1c4cf987a64b59f84eafb5d36535a97179a
Smiley <95607967+SmileyPeys@users.noreply.github.com>
2022-03-02 19:03:18 -0800

    Create ph.lua

---
+++ esx_mangofarm/locales/ph.lua
@@ -0,0 +1,15 @@
+Locales ['ph'] = {
+  -- mango
+  ['wood_pickupprompt'] = 'Pindutin ang ~INPUT_CONTEXT~ para ~Maghanap ng Manga~s~ .',
+  ['wood_inventoryfull'] = 'Puno na bulsa mo boi!'
+
+  -- etc
+  ['Enter'] = 'Pindutin ang ~INPUT_CONTEXT~ para mag linis ~INPUT_CONTEXT~ ng manga.',
+  ['EnterSell'] = 'Pindutin ang ~INPUT_CONTEXT~ para mag benta ng ~g~Sariwang Mango~s~. ',
+  ['NoItem'] = "Wala ka neto.',
+
+-- blips
+  ['map_blipF'] = 'Mango Field',
+  ['map_blipW'] = 'Wash Mango',
+  ['map_blip'] = 'Mango',
+}
